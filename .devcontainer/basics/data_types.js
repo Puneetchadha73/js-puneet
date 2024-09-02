d2e2f20363583b5618ed04b3effee39973e93c62
Puneetchadha73 <itzme.puneet@gmail.com>
2024-09-02 05:06:15 +0000

    data types examples

---
+++ .devcontainer/basics/data_types.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all code as newer version of Java
+// alert(3+3) // we are using node js and not browser
+// datatypes - number, bigint, string, boolean, null, undefined, symbol, object
+let names = "Puneet"
+let age = 20
+let is_logged = true
+
+// console.log(typeof null) --> object
+// console.log(typeof undefined) --> undefined

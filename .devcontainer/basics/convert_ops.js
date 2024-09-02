36d7191102b39c3415d66ad0e4faac56c8da796b
Puneetchadha73 <itzme.puneet@gmail.com>
2024-09-02 06:12:53 +0000

    conversion operations

---
+++ .devcontainer/basics/convert_ops.js
@@ -0,0 +1,17 @@
+let score = "100a"
+
+console.log(score)
+console.log(typeof score)
+
+let scoreInNumber = Number(score)
+console.log(scoreInNumber)
+console.log(typeof scoreInNumber)
+
+// "33" --> 33
+// "33ab" --> NAN(Not a number) but type is a number
+// True --> 1
+// False -- 0
+// 1 --> true
+// 0 --> false
+// "" --> false
+// "abc" --> true

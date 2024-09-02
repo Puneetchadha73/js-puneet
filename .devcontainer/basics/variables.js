b9d71445f31219bbb876117d8b59f5c83db51b30
Puneetchadha73 <itzme.puneet@gmail.com>
2024-09-02 02:30:40 +0000

    Variables test

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 3033
+let email = "itzme.puneet@gmail.com"
+var accountPassword = "abcd1234"
+accountCity = "New Delhi"
+
+//accountId = 3035
+email = "abc@gmail.com"
+accountPassword = "123456789"
+accountCity = "Jammu"
+console.log(accountId);
+console.log(email);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountId, email, accountPassword, accountCity])

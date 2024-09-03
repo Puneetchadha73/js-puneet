9ebbf477fb5c37e1237154d0401dd239ec1fddd0
Puneetchadha73 <itzme.puneet@gmail.com>
2024-09-03 10:10:32 +0000

    data type summary

---
+++ .devcontainer/basics/prim_non_prim_data.js
@@ -0,0 +1,37 @@
+// primitive : 7 types
+// String
+// Number
+// Boolen
+// Null
+// Undefined
+// Symbol
+// BigInt
+
+// Nn-primitive(Reference)
+// Array
+// Objects
+// Functions
+
+// Array excample
+const heros = ["Iron-man","Captain America","Spider-man","Wolverine"]
+console.log(heros[0])
+console.log(heros[1])
+
+//Object example
+let object1 = 
+{
+    name : "Puneet",
+    age : 10,
+    profession : "S/E"
+}
+
+console.log(object1.name)
+console.log(object1.profession)
+
+
+// Function example
+const myFunction = function()
+{
+console.log("Puneet is a S/E")
+}
+console.log(typeof myFunction)

d0153729c6ced77b932e55ee6da3e2a8b8007813
Puneetchadha73 <itzme.puneet@gmail.com>
2024-09-03 07:42:39 +0000

    compare_ops

---
+++ .devcontainer/basics/comparision_ops.js
@@ -0,0 +1,7 @@
+let val1 = 100
+let val2 = 101
+console.log(val1<=val2)
+
+
+// == and === are different.
+// == converts data-types while comparing while === checks the type as well and does not convert

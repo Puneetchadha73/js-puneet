bf18689b820aeddf42baf588215221801941c473
Puneetchadha73 <itzme.puneet@gmail.com>
2024-09-05 09:23:47 +0000

    string functions

---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,24 @@
+let name1 = "Puneet"
+let name2 = "Chadha"
+name3 = name1 + " " + name2
+console.log(name3)
+//console.log('Hello all My name is ${name3}')
+
+let gameName = new String("Smart boy")
+console.log(gameName[2])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(4))
+console.log(gameName.endsWith("t"))
+console.log(gameName.charAt(6))
+console.log(gameName.indexOf('S'))
+console.log(gameName.substring(0,3))
+console.log(gameName.slice(-8,4))
+
+let getName1 = ("  Jammu    ")
+console.log(getName1)
+console.log(getName1.trim())
+
+console.log(getName1.replace(" ",1))
+console.log(getName1.replace("J",1))

4ec9963c53f519b1a1321dc43cf9d2874d374c2a
Puneetchadha73 <itzme.puneet@gmail.com>
2024-09-10 09:13:25 +0000

    random function

--- .devcontainer/basics/nums_maths.js
+++ .devcontainer/basics/nums_maths.js
@@ -23,3 +23,9 @@ console.log(Math.ceil(4.2))
 console.log(Math.floor(4.8))
 console.log(Math.min(2,3,4,5,6))
 console.log(Math.max(2,3,4,5,6))
+console.log(Math.random())
+console.log(((Math.random()*10)) + 1)
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
